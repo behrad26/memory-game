56ab84a23096bef56442f985222e00ac7e67ffc3
Behrad Tondi <112078003+behrad26@users.noreply.github.com>
2023-08-06 17:47:11 +0330

    Update memory-game.py

--- memory-game.py
+++ memory-game.py
@@ -7,34 +7,35 @@ def print_table(list):
             print(str(j) + ((length - len(str(j))) * " "), end = "")
         print()
 
-width, height = int(input("Width: ")), int(input("Height: "))
+width, height = int(int(input("Rows: ")), input("Columns: "))
 length, grid = len(str(width * height)) + 2, list(range(1, (width * height) + 1))
 shuffle(grid)
 unique = grid.copy()
 
-grid = [grid[i:i+width] for i in range(0, len(grid), width)]
-unique = [unique[i:i+width] for i in range(0, len(unique), width)]
+grid, unique = [grid[i:i+width] for i in range(0, len(grid), width)], [unique[i:i+width] for i in range(0, len(unique), width)]
 
 row, col = randint(0, height - 1), randint(0, width - 1)
 random_number = grid[row][col]
 
 print_table(grid)
-grid[row][col] = "X"
+grid[row][col] = "O"
 input("Press enter to hide the table.")
 for i in range(0, len(grid)):
     for j in range(0, len(grid[i])):
         if i != row or j != col:
-            grid[i][j] = "O"
+            grid[i][j] = "X"
 
-for i in range(5, 0, -1):
-    if i == 5: print("\033[F\x1b[2K" * (height + 1) + "Get ready in 5 second(s)!")
-    else: print(f"\033[FGet ready in {i} second(s)!")
-    sleep(1)
+c = 5.0
+while c > 0:
+    if c == 5.00: print("\033[F\x1b[2K" * (height + 1) + "Get ready in 5.00 second(s)!")
+    else: print(f"\033[FGet ready in {c} second(s)!")
+    c -= 0.01
+    sleep(0.01)
+
 print("\033[F\x1b[2K" * (height), end = "")
 print_table(grid)
 
-answer = int(input("Enter X: "))
-if answer == random_number: print("\033[F\x1b[2K" * (height + 1) + f"You Won!!!\nthe number is {random_number}")
-else: print("\033[F\x1b[2K" * (height + 1) + f"Sorry, the namber is {random_number}\n")
+if int(input("Enter O: ")) == random_number: print("\033[F\x1b[2K" * (height + 1) + f"You Won!!!\nthe number is {random_number}")
+else: print("\033[F\x1b[2K" * (height + 1) + f"Sorry, the number is {random_number}\n")
 print("This is the main table:")
 print_table(unique)
